3344eb04a3e2ab8857aa8fcf2be319197a812b98
Alexey Yakushin <alex.aka.ion@gmail.com>
2020-10-14 22:14:20 +0300

    fix lint

--- src/js/app.js
+++ src/js/app.js
@@ -3,8 +3,8 @@ import health from './basic';
 
 console.log('worked');
 
-console.log(health({name: 'Mar', health: 90}));
+console.log(health({ name: 'Mar', health: 90 }));
 
-console.log(health({name: 'Alx', health: 40}));
+console.log(health({ name: 'Alx', health: 40 }));
 
-console.log(health({name: 'Xxx', health: 10}));
+console.log(health({ name: 'Xxx', health: 10 }));
